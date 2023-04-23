138bf70d48779f86329b4487f159d9dc1afe7c4f
0x00 <checkmaster0@gmail.com>
2023-04-23 15:11:17 +0200

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,6 +1,6 @@
 #include <Windows.h>
 #include <stdio.h>
-#include "header.h"
+#include "ioctl.h"
 
 int main(){
     HANDLE hDriver=CreateFile("\\\\.\\ofsecdrv",GENERIC_WRITE,FILE_SHARE_WRITE,NULL,OPEN_EXISTING,0,NULL);
