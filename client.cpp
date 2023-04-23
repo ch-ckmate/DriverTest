7af4d533c6e87eefd8cbd36ce00de089c29698be
0x00 <checkmaster0@gmail.com>
2023-04-23 15:10:54 +0200

    Add files via upload

---
+++ client.cpp
@@ -0,0 +1,18 @@
+#include <Windows.h>
+#include <stdio.h>
+#include "header.h"
+
+int main(){
+    HANDLE hDriver=CreateFile("\\\\.\\ofsecdrv",GENERIC_WRITE,FILE_SHARE_WRITE,NULL,OPEN_EXISTING,0,NULL);
+    if (hDriver==INVALID_HANDLE_VALUE){
+        printf("[!] %s (%d)", "Failed to open handle",  GetLastError());
+        return 1;
+    }
+    BOOL success=DeviceIoControl(hDriver,FIRST_DRIVER_IOCTL_ONE,NULL,0,NULL,0,NULL,NULL);
+    if(success){
+        printf("Done\n");
+    }else{
+        printf("Failed\n");
+    }
+    CloseHandle((hDriver));
+}
