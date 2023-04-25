59bbd2488adeff4554aa27d3a8c22f51930d9460
0x00 <checkmaster0@gmail.com>
2023-04-25 08:27:26 +0200

    Add files via upload

--- ioctl.h
+++ ioctl.h
@@ -1,3 +1,7 @@
 #pragma once
+#define ImageFileName 0x5A8 // EPROCESS::ImageFileName
+#define ActiveThreads 0x5F0 // EPROCESS::ActiveThreads
+#define ThreadListHead 0x5E0 // EPROCESS::ThreadListHead
+#define ActiveProcessLinks 0x448 // EPROCESS::ActiveProcessLinks
 #define FIRST_DRIVER_DEVICE 0x8000
 #define  FIRST_DRIVER_IOCTL_ONE CTL_CODE(FIRST_DRIVER_DEVICE,0x8000,METHOD_NEITHER,FILE_ANY_ACCESS)
